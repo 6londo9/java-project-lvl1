6140f6e5053ce6ecb919432332570a88cac370c1
Ilya <6londo9@gmail.com>
2022-08-22 14:03:24 +1100

    remove all magic numbers

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -10,48 +10,54 @@ import java.util.Scanner;
 
 public class Engine {
     private static String rightAnswer;
-	public static void engine(int a) {
-		Cli.greetings();
-		switch (a) {
-			case 2 -> Even.getCondition();
-			case 3 -> Calc.getCondition();
-			case 4 -> GCD.getCondition();
-			case 5 -> Progression.getCondition();
-			case 6 -> Prime.getCondition();
-			default -> {
-			}
-		}
-		Scanner sc = new Scanner(System.in);
-		for (var i = 0; i < 3; i++) {
-			switch (a) {
-				case 2 -> Even.even();
-				case 3 -> Calc.calc();
-				case 4 -> GCD.gcd();
-				case 5 -> Progression.progression();
-				case 6 -> Prime.prime();
-				default -> {
-				}
-			}
-			var answer = sc.next().toLowerCase();
-			switch (a) {
-				case 2 -> rightAnswer = Even.getRightAnswer();
-				case 3 -> rightAnswer = Calc.getRightAnswer();
-				case 4 -> rightAnswer = GCD.getRightAnswer();
-				case 5 -> rightAnswer = Progression.getRightAnswer();
-				case 6 -> rightAnswer = Prime.getRightAnswer();
-				default -> {
-				}
-			}
-			var wrong = "'" + answer + "' is wrong answer ;(. Correct answer was '" + rightAnswer + "'.";
-			if (answer.equals(rightAnswer) && i == 2) {
-				System.out.println("Congratulations, " + Cli.getName() + "!");
-			} else if (answer.equals(rightAnswer)) {
-				System.out.println("Correct!");
-			} else {
-				System.out.println(wrong);
-				System.out.println("Let's try again, " + Cli.getName() + "!");
-				i = 2;
-			}
-		}
-	}
+    public static void engine(int a) {
+        final int even = 2;
+        final int calc = 3;
+        final int gcd = 4;
+        final int progression = 5;
+        final int prime = 6;
+        final int gameRounds = 3;
+        Cli.greetings();
+        switch (a) {
+		    case even -> Even.getCondition();
+		    case calc -> Calc.getCondition();
+		    case gcd -> GCD.getCondition();
+		    case progression -> Progression.getCondition();
+            case prime -> Prime.getCondition();
+            default -> {
+            }
+        }
+        Scanner sc = new Scanner(System.in);
+        for (var i = 0; i < gameRounds; i++) {
+            switch (a) {
+                case even -> Even.even();
+                case calc -> Calc.calc();
+                case gcd -> GCD.gcd();
+                case progression -> Progression.progression();
+                case prime -> Prime.prime();
+                default -> {
+                }
+            }
+            var answer = sc.next().toLowerCase();
+            switch (a) {
+                case even -> rightAnswer = Even.getRightAnswer();
+                case calc -> rightAnswer = Calc.getRightAnswer();
+                case gcd -> rightAnswer = GCD.getRightAnswer();
+                case progression -> rightAnswer = Progression.getRightAnswer();
+                case prime -> rightAnswer = Prime.getRightAnswer();
+                default -> {
+                }
+            }
+            var wrong = "'" + answer + "' is wrong answer ;(. Correct answer was '" + rightAnswer + "'.";
+            if (answer.equals(rightAnswer) && i == 2) {
+                System.out.println("Congratulations, " + Cli.getName() + "!");
+            } else if (answer.equals(rightAnswer)) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println(wrong);
+                System.out.println("Let's try again, " + Cli.getName() + "!");
+                i = 2;
+            }
+        }
+    }
 }
