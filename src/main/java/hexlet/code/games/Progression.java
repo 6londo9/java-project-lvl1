52c532c0c2cab412a3c3a25846df4317b3aa353d
Ilya <6londo9@gmail.com>
2022-09-20 20:32:18 +1100

    update progression game

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -3,12 +3,13 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 import java.util.Random;
+import java.util.Arrays;
 
 public class Progression {
 
     static final int ORIGIN_OF_LENGTH = 5;
     static final int BOUND_OF_LENGTH = 11;
-    static final int ORIGIN_FOR_FIRST_NUMBER = 1;
+    static final int ORIGIN_FOR_FIRST_NUMBER = 2;
     static final int BOUND_FOR_FIRST_NUMBER = 20;
     static final int BOUND_FOR_ARRAY_LENGTH = 10;
     private static final String DESCRIPTION = "What number is missing in the progression?";
@@ -19,12 +20,15 @@ public class Progression {
         for (var i = 0; i < Engine.GAME_ROUNDS; i++) {
             int length = random.nextInt(ORIGIN_OF_LENGTH, BOUND_OF_LENGTH);
             int start = random.nextInt(ORIGIN_FOR_FIRST_NUMBER, BOUND_FOR_FIRST_NUMBER);
-            int step = random.nextInt(BOUND_FOR_ARRAY_LENGTH);
+            int step = random.nextInt(ORIGIN_FOR_FIRST_NUMBER, BOUND_FOR_ARRAY_LENGTH);
             int hiddenMemberIndex = random.nextInt(0, length - 1);
-            String[] progression = generateProgression(start, step, length);
-            arrayOfPairsQuestionsAndAnswers[i][1] = progression[hiddenMemberIndex];
-            progression[hiddenMemberIndex] = "..";
-            arrayOfPairsQuestionsAndAnswers[i][0] = String.join(" ", progression);
+            int[] progression = generateProgression(start, step, length);
+            arrayOfPairsQuestionsAndAnswers[i][1] = String.valueOf(progression[hiddenMemberIndex]);
+            String targetToReplace = arrayOfPairsQuestionsAndAnswers[i][1];
+            String replacement = "..";
+            arrayOfPairsQuestionsAndAnswers[i][0] = Arrays.toString(progression)
+                    .replace(targetToReplace, replacement)
+                    .replaceAll("\\[|\\]", "");
         }
         return arrayOfPairsQuestionsAndAnswers;
     }
@@ -33,12 +37,12 @@ public class Progression {
         return DESCRIPTION;
     }
 
-    public static String[] generateProgression(int first, int step, int progressionLength) {
-        String[] progression = new String[progressionLength];
-        progression[0] = String.valueOf(first);
+    public static int[] generateProgression(int first, int step, int progressionLength) {
+        int[] progression = new int[progressionLength];
+        progression[0] = first;
         for (var k = 1; k < progressionLength; k++) {
-            int currentPartOfProgression = Integer.parseInt(progression[k - 1]) + step;
-            progression[k] = String.valueOf(currentPartOfProgression);
+            int currentPartOfProgression = progression[k - 1] + step;
+            progression[k] = currentPartOfProgression;
         }
         return progression;
     }
